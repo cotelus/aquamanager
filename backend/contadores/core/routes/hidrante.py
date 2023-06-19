bd3e761ceb37ca30f4573b744ac545d0ec13d868
Cotelus <cotelus@gmail.com>
2023-06-19 22:19:04 +0200

    ADD: hydrant routes

--- backend/contadores/core/routes/hidrante.py
+++ backend/contadores/core/routes/hidrante.py
@@ -6,9 +6,8 @@ from core.controllers.hidrante import HydrantController
 
 def add_routes(routes: RouteTableDef):
 
-    """
-        Devuelve una lista de contadores
-    """
+    
+    # Devuelve una lista de contadores
     @routes.get('/hidrantes/')
     async def list_(request: web.Request):
         jwt_header = request.headers.get('Authorization')
@@ -16,5 +15,43 @@ def add_routes(routes: RouteTableDef):
         if jwt_header:
             contador = HydrantController()
             return web.json_response(await contador.get_list(jwt_header), status=http.HTTPStatus.OK)
+        else:
+            return web.Response(text='No se proporcionó el encabezado "jwt"', status=http.HTTPStatus.UNAUTHORIZED)
+        
+    # Devuelve una lista de contadores
+    @routes.post('/hidrantes/')
+    async def list_(request: web.Request):
+        jwt_header = request.headers.get('Authorization')
+        data = await request.json()
+        
+        if jwt_header:
+            contador = HydrantController()
+            return web.json_response(await contador.insert_hydrant(jwt_header, data), status=http.HTTPStatus.CREATED)
+        else:
+            return web.Response(text='No se proporcionó el encabezado "jwt"', status=http.HTTPStatus.UNAUTHORIZED)
+        
+    # Devuelve una lista de contadores
+    @routes.put('/hidrantes/')
+    async def list_(request: web.Request):
+        jwt_header = request.headers.get('Authorization')
+        data = await request.json()
+        
+        if jwt_header:
+            contador = HydrantController()
+            return web.json_response(await contador.edit_hydrant(jwt_header, data), status=http.HTTPStatus.OK)
+        else:
+            return web.Response(text='No se proporcionó el encabezado "jwt"', status=http.HTTPStatus.UNAUTHORIZED)
+        
+    # Devuelve una lista de contadores
+    @routes.delete('/hidrantes/')
+    async def list_(request: web.Request):
+        jwt_header = request.headers.get('Authorization')
+        data = await request.json()
+        if not 'hydrant_id' in data:
+            return web.Response(text='El id del hidrante es necesario', status=http.HTTPStatus.NOT_ACCEPTABLE)
+        
+        if jwt_header:
+            contador = HydrantController()
+            return web.json_response(await contador.delete_hydrant(jwt_header, data['hydrant_id']), status=http.HTTPStatus.OK)
         else:
             return web.Response(text='No se proporcionó el encabezado "jwt"', status=http.HTTPStatus.UNAUTHORIZED)
